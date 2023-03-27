acf031fdc9e5c87427362ecd41bf0bfc8696d022
mdazad1508 <mdazad15081999@gmail.com>
2023-03-27 21:20:01 +0530

    rudux and router setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import Home from "./components/Home";
+
 function App() {
-  return <div className="App">Hello there..</div>;
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
 }
 
 export default App;
